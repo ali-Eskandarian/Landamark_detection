1b60e12945a58d7b6a9a8406bde72f51aa38c20e
mohammad mahdi abedi <mohammadabedi1179@gmail.com>
2023-05-08 06:08:52 +0330

    land mark detection dataloader

--- landmark_detection/load_data.py
+++ landmark_detection/load_data.py
@@ -1,27 +1,38 @@
-
-class Load_data():
-    def __init__(self, labels_path):
+import glob
+from torch.utils.data import IterableDataset, DataLoader
+import PIL
+from torchvision import transforms
+class Load_data(IterableDataset):
+    def __init__(self, labels_path, images_path):
         self.labels_path = labels_path
+        self.images_path = images_path
 
     def parse_file(self):
         
-        data = {"Name" : [], "Jaw, Nose and Euler Angles" : []}
         with open(self.labels_path, 'r') as list_of_labels:
             list_of_labels = list_of_labels.read().split("\n")
             for labels in list_of_labels:
                 sub_labels = labels.split(" ")
                 label_name = sub_labels[0].split("\\")[-1]
                 njea = [float(point) for point in sub_labels[109:111] + sub_labels[33:35] + sub_labels[203:206]]
-                data["Name"].append(label_name)
-                data["Jaw, Nose and Euler Angles"].append(njea)
-                yield label_name, njea
+                #print(label_name)
+                image_path = self.images_path + "/imgs_masked/" + label_name[:-4] + "_surgical.png"
+                #print(image_path)
+                try:    
+                    image = PIL.Image.open(image_path)
+                    image_tensor = transforms.ToTensor()(image)
+                    yield image_tensor,  njea
+                except:
+                    pass
     
-    def __iter__():
-        pass
+    def __iter__(self):
+        return self.parse_file()
 
                 
 
 if __name__=="__main__":
-    load = Load_data()
-    for data in load.read_data("test_data/list.txt"):
-        print(data)
+    dataset = Load_data("train_data/list3.txt", "train_data")
+    loader = DataLoader(dataset, batch_size=16, drop_last=True)
+    print(len(glob.glob("train_data/imgs_masked/*.png")))
+    for image, label in loader:
+        print(image.shape)
