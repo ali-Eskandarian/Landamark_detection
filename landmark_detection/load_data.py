d686d8e5ecb25ca56c0611eb5d233e3ee02323cd
mohammad mahdi abedi <mohammadabedi1179@gmail.com>
2023-05-12 10:04:07 +0330

    add comment to load data file

--- landmark_detection/load_data.py
+++ landmark_detection/load_data.py
@@ -3,26 +3,32 @@ from torch.utils.data import IterableDataset, DataLoader
 import PIL
 from torchvision import transforms
 class Load_data(IterableDataset):
+    
     def __init__(self, labels_path, images_path):
-        self.labels_path = labels_path
-        self.images_path = images_path
-
-    def parse_file(self):
         
-        with open(self.labels_path, 'r') as list_of_labels:
-            list_of_labels = list_of_labels.read().split("\n")
-            for labels in list_of_labels:
-                sub_labels = labels.split(" ")
-                label_name = sub_labels[0].split("\\")[-1]
-                angles = [float(point)/360+0.5 for point in sub_labels[203:206]]
-                njea = [float(point) for point in sub_labels[108:110] + sub_labels[32:34] + angles]
-                njea = torch.tensor(njea, dtype=torch.float32)
-                image_path = self.images_path + "/imgs_masked/" + label_name[:-4] + "_surgical.png"
+        self.labels_path = labels_path                                                                      # directory of the labels
+        self.images_path = images_path                                                                      # directory of the images
+
+    def parse_file(self):                                                                                   # function to parse the example from data loader
+        
+        with open(self.labels_path, 'r') as list_of_labels:                                                 # open the labels path to extract the labels value
+            
+            list_of_labels = list_of_labels.read().split("\n")                                              # split the list of labels into a list of lines that represents the labels of each example
+            
+            for labels in list_of_labels:                                                                   # iterate over each label
+                sub_labels = labels.split(" ")                                                              # split the label elements
+                label_name = sub_labels[0].split("\\")[-1]                                                  # extract the image name from the first element of the label
+                angles = [float(point)/360+0.5 for point in sub_labels[203:206]]                            # extract the image angles from the last three elements of the label, divided by 360 plus 0.5 in order to be scaled between 0 and 1
+                njea = [float(point) for point in sub_labels[108:110] + sub_labels[32:34] + angles]         # extract the nose and jaw positions, save the with angels as a list to represent the image label
+                njea = torch.tensor(njea, dtype=torch.float32)                                              # convert the label list to tensor
+                image_path = self.images_path + "/imgs_masked/" + label_name[:-4] + "_surgical.png"         # create image path
+                
                 try:    
-                    image = PIL.Image.open(image_path)
-                    image_tensor = transforms.ToTensor()(image)
-                    yield image_tensor,  njea
-                except:
+                    image = PIL.Image.open(image_path)                                                      # open the image path
+                    image_tensor = transforms.ToTensor()(image)                                             # convert the image to tensor
+                    yield image_tensor,  njea                                                               # return the image and the label
+                
+                except FileNotFoundError:                                                                   # there are some files that are missing from masked images but there is label in the labels list
                     pass
 
     def __iter__(self):
@@ -32,4 +38,4 @@ if __name__== "__main__":
     load = Load_data("train_data/list3.txt", "train_data")
     data_loader = DataLoader(load, batch_size=16, drop_last=True)
     for image, label in data_loader:
-        print(image)
+        print(image.size())
