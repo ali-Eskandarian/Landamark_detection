ab82bfe52c7b102ddfd870c092eca8ee0d68095b
Ali Eskandarian <ali.eskandarian8@gmail.com>
2023-05-13 16:49:49 +0330

    some changes in functions in model layers cleaning code

--- landmark_detection/run.py
+++ landmark_detection/run.py
@@ -1,37 +1,40 @@
 import os
-from model import Model
+from model import landmarks_angles_detector
 from train import Train
 from load_data import Dataset_lip
 from torch.utils.data import DataLoader
+import matplotlib.pyplot as plt
 
-class Run():
-    def __init__(self, device, batch_size, number_of_epochs):
+
+class landmark_detection:
+    def __init__(self, device, batch_size, number_of_epochs, detector):
         self.device = device
         self.batch_size = batch_size
         self.number_of_epochs = number_of_epochs
+        self.detector = detector
 
-    def dataset(self):
+    def _dataset(self):
         main_folder_path = os.getcwd()
         data = Dataset_lip(r"C:\Users\ali\PycharmProjects\Lip_Movement_Detection\train_data\Train_Images_f_1.csv",
-                            r"C:\Users\ali\PycharmProjects\Lip_Movement_Detection\train_data")
+                           r"C:\Users\ali\PycharmProjects\Lip_Movement_Detection\train_data")
         data_loader = DataLoader(data, batch_size=self.batch_size, drop_last=True)
 
         return data_loader
-    
-    def model(self, train_loader, tl_model_ex=False, tl_model='resnet'):
-        if tl_model_ex:
-            model = Model(2, self.batch_size).tl_model()
-        else:
-            model = Model(2, self.batch_size)
-        model = model.to(self.device)
-        train = Train(model, self.number_of_epochs, self.device)
-        train.compile(loss_f="MSE")
-        trained_model = train.train(train_loader)
-        train.save_model("model/landmark_detection/model.pt")
 
+    def training(self, save_root, tl_model_ex=False, tl_model='resnet'):
+        train_loader = self._dataset()
+        if tl_model_ex:
+            self.detector = self.detector(2, self.batch_size).tl_model()
+        else:
+            self.detector = self.detector(2, self.batch_size)
+        self.detector = self.detector.to(self.device)
+        trainer = Train(self.detector, self.number_of_epochs, save_root, self.device)
+        trainer.compile(loss_f="MSE")
+        trained_model = trainer.train(train_loader)
+        trainer.plot_loss()
         return trained_model
 
+
 if __name__ == '__main__':
-    run = Run('cuda', 16, 30)
-    train = run.dataset()
-    model = run.model(train)
+    run = landmark_detection('cuda', 128, 30, landmarks_angles_detector)
+    model = run.training(save_root=r"C:\Users\ali\PycharmProjects\Lip_Movement_Detection\model")
--- landmark_detection/train.py
+++ landmark_detection/train.py
@@ -1,75 +1,101 @@
 import torch
 import torch.nn as nn
 from custom_loss import CustomLoss
+import matplotlib.pyplot as plt
 
-class Train():                                                  
-  
-    def __init__(self, model, number_of_epochs : int, device = 'cpu'):
 
-        self.model = model                                                                      # model to train
-        self.number_of_epochs = number_of_epochs                                                # number of training epochs
-        self.device = device                                                                    # device to train on
+class Train:
+    def __init__(self, model, number_of_epochs: int, path_saving: str, device='cpu'):
+        self.optimizer = None
+        self.loss_f_1 = None
+        self.loss_f = None
+        self.losses = None
+        self.path = path_saving
+        self.model = model  # model to train
+        self.number_of_epochs = number_of_epochs  # number of training epochs
+        self.device = device  # device to train on
 
-    def compile(self, loss_f="BCE_LL", optimizer='Adam', learning_rate=0.0001):                 # compile loss and optimizer for training
-       
+    def compile(self, loss_f="BCE_LL", optimizer='Adam',
+                learning_rate=0.0001):
+        # compile loss and optimizer for training
         self.loss_f = CustomLoss()
         if loss_f == "BCE_LL":
-            self.loss_f_1 = nn.BCEWithLogitsLoss()                                                # set BinaryCrossEntropy loss function for non normal output as operator's preference
-        
-        elif  loss_f=="BCE":
-           self.loss_f_1 = nn.BCELoss()                                                           # set BinaryCrossEntropy loss function as operator's preference for binary classification
-        
-        elif loss_f=="CE":
-            self.loss_f_1 = nn.CrossEntropyLoss()                                                 # set CrossEntropy loss function as operator's preference for multiclass classification
-        
-        elif loss_f=="MSE":
-            self.loss_f_1 = nn.MSELoss()                                                          # set Mean Squared Error loss function as operator's preference for regression problems
-        
+            self.loss_f_1 = nn.BCEWithLogitsLoss()
+            # set BinaryCrossEntropy loss function for abnormal output as
+            # operator's preference
+        elif loss_f == "BCE":
+            self.loss_f_1 = nn.BCELoss()
+            # set BinaryCrossEntropy loss function as operator's preference for binary classification
+        elif loss_f == "CE":
+            self.loss_f_1 = nn.CrossEntropyLoss()
+            # set CrossEntropy loss function as operator's preference for multiclass classification
+        elif loss_f == "MSE":
+            self.loss_f_1 = nn.MSELoss()
+            # set Mean Squared Error loss function as operator's preference for regression problems
         if optimizer == "Adam":
-           self.optimizer = torch.optim.Adam(self.model.parameters(), lr=learning_rate)         # set Adam optimizer as operator's preference
-    
-    def train(self, train_loader, verbose=True):                                                # define the training loop
-        
+            self.optimizer = torch.optim.Adam(self.model.parameters(),
+                                              lr=learning_rate)
+            # set Adam optimizer as operator's preference
+
+    def train(self, train_loader, verbose=True):
+        # define the training loop
+        self.losses = []
         for epoch in range(self.number_of_epochs):
-            total_loss = 0.0                                                                    # at the beginning of each epoch, the loss will be assigned to zer0
+            total_loss = 0.0  # at the beginning of each epoch, the loss will be assigned to zer0
             batch = 0
-            for images, labels in train_loader:                                                  # iterate through training examples
+            for images, labels in train_loader:  # iterate through training examples
                 batch += 1
-                images = images.to(self.device).to(torch.float32)                                               # save the batch of images in device memory
-                labels = labels.to(self.device)  .to(torch.float32)                                                # save the batch of labels in device memory
+                images = images.to(self.device).to(torch.float32)  # save the batch of images in device memory
+                labels = labels.to(self.device).to(torch.float32)  # save the batch of labels in device memory
 
-                preds = self.model(images)                                                      # pass the image to the model to obtain the output
-                preds = preds.squeeze_()                                                        # squeeze the outputs of the model to be more convenient to use
-                loss_p = self.loss_f_1(preds[:, :4], labels[:, :4])                             # compute loss between the training examples and the outputs of the model
-                loss_a = self.loss_f(preds[:, 4:], labels[:, 4:])
-                loss = loss_a + loss_p                               # compute loss between the training examples and the outputs of the model
-                loss.backward()                                                                 # back propagate the loss
-                self.optimizer.step()                                                           # compute the backpropagation step by optimizer function
+                predicts = self.model(images).squeeze_()
+                # pass the image to the model to obtain the output then squeeze it
+                loss_p = self.loss_f_1(predicts[:, :4], labels[:, :4])
+                # compute loss between the training examples and the outputs of the model
+                loss_a = self.loss_f(predicts[:, 4:], labels[:, 4:])
+                loss = loss_a + loss_p
+                # compute loss between the training examples and the outputs of the model
+                loss.backward()
+                # back propagate the loss
+                self.optimizer.step()
+                # compute the backpropagation step by optimizer function
                 self.optimizer.zero_grad()
-    
-                total_loss += loss.item()                                                       # add loss of each batch to the total loss of the epoch
-                if batch%1000 == 0:
-                    print(f"Loss[batch = {batch}]= {loss.item()}  , total_loss = {total_loss}")
-            if verbose==True:                                                                   
-                print(f'For the epoch number {epoch+1}: The training loss is {total_loss}.')    # if the verbose flag is set to True, print the training loss value
-        
-        return self.model                                                                       # return the trained model
-    
-    def n_parameters(model):                                                                    # compute the number of parameters
-        
+                total_loss += loss.item()
+                # add loss of each batch to the total loss of the epoch
+                if batch % 100 == 0:
+                    print(f"Loss[batch = {batch}/{len(train_loader)}]= {loss.item()}"
+                          f"  , total_loss = {total_loss}")
+            self.losses.append(total_loss)
+            self._save_(dynamic_save=True)
+            if verbose:
+                print(
+                    f'For the epoch number {epoch + 1}: The training loss is {total_loss}.')
+                # if the verbose flag is set to True, print the training loss value
+        self._save_()
+        return self.model  # return the trained model
+
+    def n_parameters(self):  # compute the number of parameters
         total_par = 0
-        for par in model.parameters():                                                          # iterate through model parameters
-            if par.requires_grad :                                                              # compute only the trainable parameters
-                total_par += par.numel()                                                        # add the number of parameters of each layer to the total number of parameters
+        for par in self.model.parameters():  # iterate through model parameters
+            if par.requires_grad:  # compute only the trainable parameters
+                total_par += par.numel()  # add the number of parameters of each layer to the total number of parameters
+        print(f"Total number of trainable parameters for this model is {total_par}")
 
-        print(f"Total number of trainable paramaters for this model is {total_par}")
+    def plot_loss(self):
+        plt.plot(self.losses)
+        plt.title('Training Loss')
+        plt.xlabel('Epoch')
+        plt.ylabel('Loss')
+        plt.savefig(self.path + r"\loss_training.png")
 
-    def save_model(self, path : str):
+    def _save_(self, dynamic_save=False):
+        # save the model parameters
+        if dynamic_save:
+            torch.save(self.model.state_dict(), self.path + r"\model_dynamic.pt")
+        else:
+            torch.save(self.model.state_dict(), self.path + r"\model.pt")
 
-        torch.save(self.model.state_dict(), path)                                               # save the model parameters
-
-    def load_model(model, path):
-        
-        model.load_state_dict(torch.load(path))                                                 # load the presaved model parameters
-
-        return model
+    def load_model(self, path):
+        self.model.load_state_dict(torch.load(path))
+        # load the preserved model parameters
+        return self.model
