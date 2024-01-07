a21766cb2c45dc8d4cb43245fd24a823ca667270
Ali <106665821+ali-Eskandarian@users.noreply.github.com>
2024-01-08 01:37:37 +0330

    Update Run.py
    ...

--- Run.py
+++ Run.py
@@ -5,12 +5,12 @@ import torch
 
 
 def main():
-    #model = run.training()
+    model = run.training()
     model = landmarks_angles_detector(16, 30, previous_model=False).to('cpu')
     summary(model, (3, 112, 112))
-    # model.load_state_dict(torch.load("model/landmark_detection/inception/pretrained/model_dynamic.pt"))
-    #run = landmark_detection('cuda', 16, 30, model, load=False)
-    #trained_model = run.training(save_root="save")
+    model.load_state_dict(torch.load("model/landmark_detection/inception/pretrained/model_dynamic.pt"))
+    run = landmark_detection('cuda', 16, 30, model, load=False)
+    trained_model = run.training(save_root="save")
 
 if __name__ == '__main__':
     main()
