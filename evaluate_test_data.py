554c35903d3d1e13a68195a13b72380dc3256ef5
Ali <106665821+ali-Eskandarian@users.noreply.github.com>
2024-01-08 01:37:08 +0330

    Update evaluate_test_data.py
    delete comments

--- evaluate_test_data.py
+++ evaluate_test_data.py
@@ -3,36 +3,6 @@ from torch.utils.data import DataLoader
 import matplotlib.pyplot as plt
 from model import landmarks_angles_detector
 from load_data import Dataset_lip
-
-# class Evaluate():
-#     def __init__(self, data):
-#         self.data = data
-#
-#     def show_test_results(self):
-#         model = landmarks_angles_detector(16, 16, previous_model=False)
-#         model.load_state_dict(torch.load("model.pt"))
-#
-#         plt.figure(figsize=(20, 20))
-#         for index, (images, labels) in enumerate(self.data, start=1):  # Start enumerate from index 1
-#             image = images[0]  # Assuming you have only one image in each batch
-#             image = image.permute(1, 2, 0)
-#
-#             pred = model(torch.unsqueeze(images[0] / 255, 0))
-#             pred = pred.squeeze()
-#
-#             plt.subplot(4, 4, index)  # Update the subplot index
-#
-#             plt.imshow(image[int(112 * pred[0].item()):int(112 * pred[2].item()), :, :])
-#             plt.axis('off')
-#             if index == 16:
-#                 break
-#         plt.show()
-#         plt.savefig("a.png")
-import torch
-from torch.utils.data import DataLoader
-import matplotlib.pyplot as plt
-from model import landmarks_angles_detector
-from load_data import Dataset_lip
 a, b  = 50, 35
 class Evaluate():
     def __init__(self, data):
